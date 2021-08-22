7947c3c5479707c2975e054973fb14d7bf67e864
unknown <cristiano.enke@gmail.com>
2021-08-22 12:19:41 -0300

    Javascript - React - Fragments

---
+++ 4 - Fragments/src/App.js
@@ -0,0 +1,18 @@
+import React, {Fragment} from 'react'
+
+
+function App(){
+
+  return (
+    <Fragment>
+    <div>
+      React
+    </div>
+    <div>
+    React.JS
+    </div>
+    </Fragment>
+  )
+}
+
+export default App
